e9ba5db8a1655fe8069b8fa611a7d68282735acc
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-17 03:24:53 +0100

    Recuerdos/olvidar

--- Recuerdos/Recuerdos.py
+++ Recuerdos/Recuerdos.py
@@ -8,13 +8,23 @@ recordar = 0
 
 while option != "q":
     option = input(
-        "Qué quieres hacer? \n     1-Crear recuerdo        2-Recordar      q-Salir\n")
+        "Qué quieres hacer? \n     1-Crear recuerdo        2-Recordar      3-Olvidar        q-Salir\n")
     if option == "1":
         recuerdo = input("Crea el recuerdo: \n")
         Recuerdos.append(recuerdo)
     if option == "2":
-        recordar = int(input("Qué recuerdo quieres? (1,2,3...)\n"))
+        recordar = int(input("Qué recuerdo quieres recordar? (1,2,3...)\n"))
         try:
             print(Recuerdos[recordar-1])
+        except:
+            print("Este recuerdo no existe... :c")
+    if option == "3":
+        recordar = int(input("Qué recuerdo quieres olvidar? (1,2,3...)\n"))
+
+        try:
+            print("El recuerdo nº"+str(recordar) +
+                  " se ha borrado, el recuerdo nº"+str(recordar+1)+" ocupará su lugar!")
+            Recuerdos.remove(Recuerdos[recordar-1])
+
         except:
             print("Este recuerdo no existe... :c")
